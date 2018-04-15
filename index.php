7b75b4c8248c34144ec8599e0c846ebbd7f18070
djhyv2 <djhyv2@mail.missouri.edu>
2018-04-15 17:32:50 -0500

    Added a line break

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?php
-    echo "Magic Match Tracker\nCreated by: Dustin Hengel, Mercy Housh, Connor Fitzmaurice, and Scott";
+    echo "Magic Match Tracker\n<br>"
+. " Created by: Dustin Hengel, Mercy Housh, Connor Fitzmaurice, and Scott";
     
 ?>
 
