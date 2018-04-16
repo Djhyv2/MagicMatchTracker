63bbb384d5a35612da4151dc3c57c6fc35749a79
djhyv2 <djhyv2@mail.missouri.edu>
2018-04-15 22:58:48 -0500

    Added connection to Database, working on Master because nothing yet exists to break in master.

---
+++ credentials.php
@@ -0,0 +1,8 @@
+<?php
+    $server="sql206.epizy.com";
+    $database="epiz_21939884_MagicMatch";
+    $user="epiz_21939884";
+    $password="bcIwL6nkfR3b";
+    //Stores information to connect to database
+?>
+
