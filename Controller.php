7833a938ba62dd77e665a3bf6c76f7225a8c9bb6
Housh <mehz76@tc-m610-login-node623.cobalt.lan>
2018-04-18 22:49:55 -0500

    Initial draft of the controller

--- Controller.php
+++ Controller.php
@@ -7,22 +7,113 @@
     {
         private $model;//Model Component
         private $view;//View Component
+
+        private $action = '';
+        private $message = '';
+        private $data = array();
+
         public function __construct()
         {
-            $model= new Model();
-            $view= new View();//Creates Model and View
+            $this->model= new Model();
+            $this->view= new View();//Creates Model and View
+            $this->view = $_GET['view'] ? $_GET['view'] : 'gameList';
+            this->action = $_POST['action'];
         }//Constructor for Controller
 
         public function __destruct() 
         {
-             $model=null;
-             $view=null;//Deletes Model and View
+             $this->model = null;
+             $this->view = null;//Deletes Model and View
         }//Desructor for Controller
 
         public function run()
         {
+            if($error = $this->model->getError()) {
+                print $view->errorView($error);
+                exit;
+            }
+
+            $this->processOrderBy();
+
+            switch($this->action) {
+                case 'delete':
+                    $this->handleDelete();
+                    break;
+                case 'add':
+                    $this->handleAddGame();
+                    break;
+                case 'edit':
+                    $this->handleEditGame();
+                    break;
+                case 'update':
+                    $this->handleUpdateGame();
+                    break;
+            }
+
+            switch($this->view) {
+                case 'gameForm':
+                    print $this->view->gameFormView($this->data, $this->message);
+                    break;
+                 default:
+                    list($orderBy, $orderDirection) = $this->model->getOrdering();
+                    if($error) {
+                        $this->message = $error;
+                    }
+                    print $this->view->gameListView($games, $orderBy, $orderDirction, $this->message);
+            }
 
         }//Main Function for Controller, Ran at Beginning of Program
+
+        private function processOrderBy() {
+            if($_GET['orderby']) {
+                $this->model->toggleOrder($_GET['orderby']);
+            }
+        }
+
+        private function handleDelete() {
+            if($ettot = $this->model->deleteGame($_POST['id'])) {
+                $this->message = $error;
+            }
+            $this->view = 'gameList';
+        }
+
+        private function handleAddGame() {
+            if($_POST['cancel']) {
+                $this->view = 'gameList';
+                return;
+            }
+            $error = $this->model->addGame($_POST);
+            if($error) {
+                $this->message = $error;
+                $this->view = 'gameForm';
+                $this->data= $_POST;
+            }
+        }
+
+        private function handleEditGame() {
+            list($task, $error) = $this->model->getGame($_POST['id']);
+            if($error) {
+                $this->message = $error;
+                $this->view = 'gameList';
+                return;
+            }
+            $this->data = $game;
+            $this->view = 'gameForm';
+        }
+
+        private function handleUpdateGame() {
+            if($_POST['cancel']) {
+                $this->view = 'gameList';
+                return;
+            }
+            if($error = $this->model->updateGame($-POST)) {
+                $this->message = $error;
+                $this->view = 'gameForm';
+                $this->data = $_POST;
+                return;
+            }
+            $this->view = 'gameList';
+        }
     }
 ?>
 
