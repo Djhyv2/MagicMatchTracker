3709aec6065d555d486e6e67513e3d03561074b3
djhyv2 <djhyv2@mail.missouri.edu>
2018-04-19 20:32:39 -0500

    Read Single Match Function Complete

--- Model.php
+++ Model.php
@@ -19,7 +19,7 @@
             $sql=null;
         }//Destructor for Model
         
-        public function read()
+        public function readMatches()
         {
             $matches=array();//Array to Store all Matches
             $error="";//Holds error string
@@ -54,6 +54,62 @@
             }//Else Connection Error
             return array($matches,$error);//Returns matches and possible error string
         }//Reads Matches from Database
+        
+        public function readMatch($id)
+        {
+            $match=NULL;//Holds Match to be Returned
+            $error="";//Holds error string
+            if($id!=null)
+            {
+                if($sql->connect_error==null)
+                {
+                    $preparedStatement = $sql->prepare('
+                        SELECT Player1.FirstName AS "First Name", Player1.LastName AS "Last Name", Player1Deck.Name AS "Deck", Player2.FirstName AS "Opponent First Name",Player2.LastName AS "Opponent Last Name", Player2Deck.Name AS "Opponent Deck", Matches.Wins ,Matches.Losses, Matches.Ties, Matches.Date,Matches.Tournament 
+                        FROM Matches
+                        JOIN Players AS Player1 ON Matches.Player1ID = Player1.ID
+                        JOIN Players AS Player2 ON Matches.Player2ID = Player2.ID
+                        JOIN Decks AS Player1Deck ON Matches.Player1DeckID = Player1Deck.ID
+                        JOIN Decks AS Player2Deck ON Matches.Player2DeckID = Player2Deck.ID
+                        WHERE Matches.ID = ?;');//Prepares statement to inject ID into
+                    if($preparedStatement->bind_param("i",$id)==true)
+                    {
+                        if($preparedStatement->execute()==true)
+                        {
+                            $result=$preparedStatement->get_result();//Gets Result from Query
+                            if($result!=null)
+                            {
+                                if($result->num_rows==1)
+                                {
+                                    $match=$result->fetch_assoc();//Gets Match with given ID
+                                }//If Exactly One Result
+                                $preparedStatement->close();//Closes Statement
+                            }//If Result Retrieved
+                            else
+                            {
+                                $error=$preparedStatement->error;
+                            }//Else Failed to Retrieve Result   
+                        }//If Successfully Executed Query
+                        else
+                        {
+                            $error=$preparedStatement->error;
+                        }//Else Failed to Execute Query
+                    }//If Successfully Bound Parameter to Prepared Statement
+                    else
+                    {
+                        $error = $sql->error;
+                    }//Else didn't bind parameter
+                }//If No Connection Error
+                else
+                {
+                    $error=$sql->connect_error;
+                }//Else Connection Error
+            }//If ID Provided
+            else
+            {
+                $error="No ID Provided";
+            }//Else No ID
+            return array($match,$error);//Returns matches and possible error string   
+        }
         
         public function getError()
         {
