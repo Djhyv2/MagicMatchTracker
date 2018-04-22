c9cfeb5262c8735a70c3c57f7daa121650cdbbe0
Scott Watkins <watkinssr@outlook.com>
2018-04-22 17:18:58 -0500

    Continued work on matchListView function

--- View.php
+++ View.php
@@ -4,20 +4,18 @@
         private $stylesheet = 'style.css'; //variable to store stylesheet
         private $pageTitle = 'Magic Match Tracker'; //varable to store page title; useful for changing page title without hardcoding
 
-        public function __construct() 
-        {
+        public function __construct () {
             echo "Magic Match Tracker\n<br>Created by: Dustin Hengel, Mercy Housh, Connor Fitzmaurice, and Scott Watkins";//Prints to Standard Out
         }//Constructor for View
         
-        public function __destruct()
-        {
+        public function __destruct () {
             
         }//Destructor for View
 
-        public function matchListView($matches, $orderby = 'wins', $orderDirection = 'desc', $message = ''){ //default order by wins descending
+        public function matchListView ($matches, $orderby = 'wins', $orderDirection = 'desc', $message = ''){ //default order by wins descending
             $body = "<h1>Magic Match Tracker</h1>\n"; //adds the top of the page title to the $body variable to be displayed
 
-            if($message) { //if a message has been passed in, then it will be displayed
+            if ($message) { //if a message has been passed in, then it will be displayed
                 $body .= "<p class='message'>$message</p>\n";
             }
         
@@ -31,13 +29,33 @@
             $body .= "<table>\n"; //open table tag
             $body .= "<tr><th>delete</th><th>edit</th>"; //html for opening row tag, as well as delete and edit columns
             
-            $columns = array(
-                array('name' => 'player1', 'label' => 'Player 1'),
-                array('name' => 'player2', 'label' => 'Player 2'),
-                array('name' => 'wins', 'label' => 'Wins'),
-                array('name' => 'losses', 'label' => 'Losses'),
-                array('name' => 'ties', 'label' => 'Ties')
+            $columns = array (
+                array ('name' => 'player1', 'label' => 'Player 1'),
+                array ('name' => 'player2', 'label' => 'Player 2'),
+                array ('name' => 'wins', 'label' => 'Wins'),
+                array ('name' => 'losses', 'label' => 'Losses'),
+                array ('name' => 'ties', 'label' => 'Ties')
             );
+
+            foreach ($columns as $column) {
+                $name = $column['name'];
+                $label = $column['label'];
+                
+                if ($name == $orderby) {
+                    if ($orderDirection == 'asc') {
+                        $label .= " &#x25BC;"; // ▼                    
+                    } else {
+                        $label .= " &#x25B2;"; // ▲
+                    }
+                }
+                
+                $body .= "<th><a class='order' href='index.php?orderby=$name'>$label</a></th>";
+            }
+
+            foreach ($matches as $match) { //need a better idea of table schema and desired data to be displayed?
+                $id = $match['id'];
+
+            }
         }
     }  
 ?>
