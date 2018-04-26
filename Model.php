b1e3224731692262f3dd54684bfda4f1175385eb
djhyv2 <djhyv2@mail.missouri.edu>
2018-04-26 17:34:49 -0500

    Error Checking for Update Match

--- Model.php
+++ Model.php
@@ -74,13 +74,12 @@
             
                 
             $preparedStatement = $sql->prepare('
-                    SELECT Matches.Format, Player1.FirstName AS "First Name", Player1.LastName AS "Last Name", Player2.FirstName AS "Opponent First Name",Player2.LastName AS "Opponent Last Name", Player1Deck.Name AS "Deck", Player2Deck.Name AS "Opponent Deck", Matches.Wins ,Matches.Losses, Matches.Ties, Matches.Date,Matches.Tournament 
+                    SELECT Matches.ID, Matches.Player1ID AS Player1ID, Matches.Player2ID AS Player2ID, Matches.Player1DeckID AS Player1DeckID, Matches.Player2DeckID AS Player2DeckID, Format, Player1.FirstName AS "First Name", Player1.LastName AS "Last Name", Player2.FirstName AS "Opponent First Name",Player2.LastName AS "Opponent Last Name", Player1Deck.Name AS "Deck", Player2Deck.Name AS "Opponent Deck", Matches.Wins ,Matches.Losses, Matches.Ties, Matches.Date,Matches.Tournament 
                     FROM Matches
                     JOIN Players AS Player1 ON Matches.Player1ID = Player1.ID
                     JOIN Players AS Player2 ON Matches.Player2ID = Player2.ID
                     JOIN Decks AS Player1Deck ON Matches.Player1DeckID = Player1Deck.ID
-                    JOIN Decks AS Player2Deck ON Matches.Player2DeckID = Player2Deck.ID
-                    WHERE Matches.ID = ?; ');//Prepares statement to inject ID into
+                    JOIN Decks AS Player2Deck ON Matches.Player2DeckID = Player2Deck.ID');//Prepares statement to inject ID into
   
             if($preparedStatement->bind_param("i",$id)==false)
             {
@@ -181,7 +180,8 @@
             }
             //Returns Error if Missing Critical Info
             
-            $player1ID=addPlayer($player1FirstName,$player1LastName);
+            
+            $player1ID=addPlayer($player1FirstName,$player1LastName);//Adds Player1 or Gets Player1ID
             if($error!=NULL)
             {
                 return $error;
@@ -189,7 +189,7 @@
             $player2ID=NULL;//Default Player2ID to NULL
             if($player2FirstName!=""||$player2LastName!="")
             {
-                $player2ID=addPlayer($player2FirstName,$player2LastName);
+                $player2ID=addPlayer($player2FirstName,$player2LastName);//Adds Player2 or Gets Player2ID
                 if($error!=NULL)
                 {
                     return $error;
@@ -197,18 +197,18 @@
             }//If Player2 Information Exists
             
             
-            $player1DeckID=addDeck($player1DeckName,$player1MainBoard,$player1SideBoard);
+            $player1DeckID=addDeck($player1DeckName,$player1MainBoard,$player1SideBoard);//Adds Player1Deck or Gets Player1DeckID
             if($error!=NULL)
             {
                 return $error;
             }//If addDeck errored
-            $player2DeckID=addDeck($player2DeckName,$player2MainBoard,$player2SideBoard);
+            $player2DeckID=addDeck($player2DeckName,$player2MainBoard,$player2SideBoard);//Adds Player2Deck or Gets Player2DeckID
             if($error!=NULL)
             {
                 return $error;
             }//If addDeck errored
 
-            $preparedStatement=$sql->prepare('INSERT INTO Matches (Player1ID,Player2ID,Wins,Losses,Ties,Player1DeckID,Player2DeckID,Date,Tournament,Format) VALUES (?,?,?,?,?,?,STR_TO_DATE(?,"%m-%d-%y"),?,?)');//Prepares Insert
+            $preparedStatement=$sql->prepare('INSERT INTO Matches (Player1ID,Player2ID,Wins,Losses,Ties,Player1DeckID,Player2DeckID,Date,Tournament,Format) VALUES (?,?,?,?,?,?,STR_TO_DATE(?,"%m-%d-%y"),?,?)');//Prepares Match Insert
             if($preparedStatement->bind_param("iiiiiiisis",$player1ID,$player2ID,$wins,$losses,$ties,$player1DeckID,$player2DeckID,$date,$tournament,$format)==false)
             {
                 $error = $sql->error;
@@ -228,7 +228,7 @@
         
         private function addPlayer($firstName,$lastName)
         {
-            $preparedStatement=$sql->prepare('INSERT IGNORE INTO Players (FirstName,LastName) VALUES (?,?)');//Prepares Insert
+            $preparedStatement=$sql->prepare('INSERT IGNORE INTO Players (FirstName,LastName) VALUES (?,?)');//Inserts if Not Duplicate Player
             if($preparedStatement->bind_param("ss",$firstName,$lastName)==false)
             {
                 $error = $sql->error;
@@ -245,7 +245,7 @@
             
             
             
-            $preparedStatement=$sql->prepare('SELECT ID FROM Players WHERE FirstName = ? AND LastName = ?');//Prepares Select
+            $preparedStatement=$sql->prepare('SELECT ID FROM Players WHERE FirstName = ? AND LastName = ?');//Gets ID
             if($preparedStatement->bind_param("ss",$firstName,$lastName)==false)
             {
                 $error = $sql->error;
@@ -280,7 +280,7 @@
         
         private function addDeck($name,$mainBoard,$sideBoard)
         {
-            $preparedStatement=$sql->prepare('SELECT ID FROM Decks WHERE Name = ? AND Mainboard = ? AND Sideboard = ?');//Prepares Select
+            $preparedStatement=$sql->prepare('SELECT ID FROM Decks WHERE Name = ? AND Mainboard = ? AND Sideboard = ?');//Selects Deck ID if Exists
             if($preparedStatement->bind_param("sss",$name,$mainBoard,$sideBoard)==false)
             {
                 $error = $sql->error;
@@ -307,7 +307,7 @@
             }//If Deck was in Database
             
             
-            $preparedStatement=$sql->prepare('INSERT INTO Decks (Name,Mainboard,Sideboard) VALUES (?,?,?);');//Prepares Insert
+            $preparedStatement=$sql->prepare('INSERT INTO Decks (Name,Mainboard,Sideboard) VALUES (?,?,?);');//Inserts if DeckID didn't Exist
             if($preparedStatement->bind_param("sss",$name,$mainBoard,$sideBoard)==false)
             {
                 $error = $sql->error;
@@ -322,7 +322,7 @@
             
             $preparedStatement->close();//Closes Statement
             
-            $results = $sql->query('SELECT LAST_INSERT_ID()');//Queries SQL Database to Read Last Insert ID
+            $results = $sql->query('SELECT LAST_INSERT_ID()');//Gets Newly Inserted ID
             
             if($result==null)
             {
@@ -341,6 +341,102 @@
             $results->close();//Closes results
             
             return $id;//Returns Deck ID
+        }
+        
+        public function updateMatch($matchID)
+        {
+            if($matchID==null)
+            {
+                $error="No MatchID";
+                return $error;
+            }//If No MatchID
+            
+            if($sql->connect_error!=null)
+            {
+                $error=$sql->connect_error;
+                return $error;
+            }//If Connection Error
+            
+            $player1FirstName=$match['player1FirstName'];
+            $player1LastName=($match['player1LastName']!=NULL?$match['player1LastName']:"");//Default Blank if Not Specified
+            $player1DeckName=$match['player1DeckName'];
+            $player1MainBoard=($match['player1MainBoard']!=NULL?$match['player1MainBoard']:"");//Default Blank if Not Specified
+            $player1SideBoard=($match['player1SideBoard']!=NULL?$match['player1SideBoard']:"");//Default Blank if Not Specified
+            $player2FirstName=($match['player2FirstName']!=NULL?$match['player2LastName']:"");//Default Blank if Not Specified
+            $player2LastName=($match['player1LastName']!=NULL?$match['player2LastName']:"");//Default Blank if Not Specified
+            $player2DeckName=$match['player2DeckName'];
+            $player2MainBoard=($match['player2MainBoard']!=NULL?$match['player2MainBoard']:"");//Default Blank if Not Specified
+            $player2SideBoard=($match['player2SideBoard']!=NULL?$match['player2SideBoard']:"");//Default Blank if Not Specified
+            $wins=$match['wins'];
+            $losses=$match['losses'];
+            $ties=($match['ties']!=NULL?$match['ties']:0);//Default 0 Ties is not specified
+            $date=$match['date'];
+            $tournament=($match['tournament']!=NULL?$match['tournament']:0);//Default Not a Tournament if not specified
+            $format=($match['format']!=NULL?match['format']:'Modern');//Default Modern if not specified, Gets Data input from Parameter
+            $player1ID=$match['player1ID'];
+            $player2ID=($match['player2ID']!=NULL?$match['player2ID']:"");//Default Blank if Not Specified
+            $player1DeckID=$match['player1DeckID'];
+            $player2DeckID=$match['player2DeckID'];
+
+            if($player1FirstName==NULL)
+            {
+                $error="Missing Player 1 First Name";
+                return $error;
+            }
+
+            if($player1DeckName==NULL)
+            {
+                $error="Missing Player 1 Deck Name";
+                return $error;
+            }
+
+            if($player2DeckName==NULL)
+            {
+                $error="Missing Player 2 Deck Name";
+                return $error;
+            }
+
+            if($wins==NULL)
+            {
+                $error="Missing Wins";
+                return $error;
+            }
+
+            if($losses==NULL)
+            {
+                $error="Missing Losses";
+                return $error;
+            }
+
+            if($date==NULL)
+            {
+                $error="Missing Date";
+                return $error;
+            }
+            
+            if($player1ID==NULL)
+            {
+                $error="Missing Player 1 ID";
+                return $error;
+            }
+            
+            if($player1DeckID==NULL)
+            {
+                $error="Missing Player 1 DeckID";
+                return $error;
+            }
+            
+            if($player2DeckID==NULL)
+            {
+                $error="Missing Player 2 DeckID";
+                return $error;
+            }
+            //Returns Error if Missing Critical Info
+            
+            
+            
+            
+            
         }
         
     }
