c0fee19cef2ae9085020703678891588a4f817a0
Scott Watkins <watkinssr@outlook.com>
2018-04-28 10:18:13 -0500

    Changed Index File

--- Index.php
+++ Index.php
@@ -1,5 +1,20 @@
 <?php
+    echo "If you see this you index works!";
+    
+    require('Credentials.php');
     require("Controller.php");
+
+    $mysqli = new mysqli($server,$user,$password,$database);
+    if($mysqli->connect_error)
+    {
+        echo $mysqli->connect_error;
+    }
+    else
+    {
+        echo mysqli_stat($mysqli);
+        echo "If you see this I can access your database!";
+    }
+    
     $controller=new Controller();
     $controller->run();
 ?>
