cadf08d6ab5844eb6f84f66f4ad720670ed5745e
djhyv2 <djhyv2@mail.missouri.edu>
2018-04-28 18:35:13 -0500

    Reduced Index.php

--- index.php
+++ index.php
@@ -1,21 +1,7 @@
 <?php
-    echo "If you see this the index works!<br><br>";
-    
-    require('Credentials.php');
     require("Controller.php");
 
-    $mysqli = new mysqli($server,$user,$password,$database);
-    if($mysqli->connect_error)
-    {
-        echo $mysqli->connect_error;
-    }
-    else
-    {
-        echo mysqli_stat($mysqli);
-        echo "<br><br>If you see this I can access your database!<br><br>";
-    }
-    
     $controller=new Controller();
-    $controller->run();
+    $controller->run();//Creates and Runs Controller
 ?>
 
