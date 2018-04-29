44dd2ebae23df06297a0318a116fec62423b4806
djhyv2 <djhyv2@mail.missouri.edu>
2018-04-28 20:50:43 -0500

    Fixed readMatch and readMatches

--- Model.php
+++ Model.php
@@ -3,7 +3,7 @@
     class Model
     {
         private $sql;
-        private $error;
+        private $error=NULL;
         
         public function __construct()
         {
@@ -24,6 +24,14 @@
         public function readMatches()
         {
             $matches=array();//Array to Store all Matches
+            
+            if($this->sql==NULL)
+            {
+                $this->error="No Database Connection";
+                return array($matches,$this->error)
+            }//If No Connection to Database
+            
+            
             if($this->sql->connect_error!=null)
             {
                 $this->error=$this->sql->connect_error;
@@ -31,12 +39,10 @@
             }//If Connection Error
             
             $results = $this->sql->query('
-                    SELECT Matches.ID, Matches.Format, Player1.FirstName AS "First Name", Player1.LastName AS "Last Name", Player2.FirstName AS "Opponent First Name",Player2.LastName AS "Opponent Last Name", Player1Deck.Name AS "Deck", Player2Deck.Name AS "Opponent Deck", Matches.Wins ,Matches.Losses, Matches.Ties, Matches.Date,Matches.Tournament 
+                    SELECT Matches.ID, Matches.Format, Player1Table.Username AS "Player1", Player2Table.Username AS "Player2", Player1Table.DeckName AS "Player1 Deck", Player1Table.DeckLink AS "Player1 Deck Link", Player2Table.DeckName AS "Player2 Deck", Player2Table.DeckLink AS "Player2 Deck Link", Matches.Wins ,Matches.Losses, Matches.Ties, Matches.Date, Matches.Tournament 
                     FROM Matches
-                    JOIN Players AS Player1 ON Matches.Player1ID = Player1.ID
-                    JOIN Players AS Player2 ON Matches.Player2ID = Player2.ID
-                    JOIN Decks AS Player1Deck ON Matches.Player1DeckID = Player1Deck.ID
-                    JOIN Decks AS Player2Deck ON Matches.Player2DeckID = Player2Deck.ID; ');//Queries SQL Database to Read all Matches
+                    JOIN MatchParts AS Player1Table ON Player1Table.MatchID = Matches.ID AND Player1Table.OrderedFirst = 1
+                    JOIN MatchParts AS Player2Table ON Player2Table.MatchID = Matches.ID AND Player2Table.OrderedFirst = 0;');//Queries SQL Database to Read all Matches
             
             if($results==null)
             {
@@ -44,7 +50,7 @@
                 return array($matches,$this->error);//Returns Empty matches and Error
             }//If Query Errored
             
-            if($result->num_rows > 0)
+            if($results->num_rows > 0)
             {
                 for($record=$results->fetch_assoc();$record!=null;$record=$results->fetch_assoc())
                 {
@@ -60,11 +66,18 @@
         public function readMatch($id)
         {
             $match=NULL;//Holds Match to be Returned
+            
             if($id==null)
             {
                 $this->error="No ID Provided";
                 return array($match,$this->error);//Returns empty match and error string
             }//If No ID
+            
+            if($this->sql==NULL)
+            {
+                $this->error="No Database Connection";
+                return array($matches,$this->error)
+            }//If No Connection to Database
             
             if($this->sql->connect_error!=null)
             {
@@ -74,12 +87,11 @@
             
                 
             $preparedStatement = $this->sql->prepare('
-                    SELECT Matches.ID, Matches.Player1ID AS Player1ID, Matches.Player2ID AS Player2ID, Matches.Player1DeckID AS Player1DeckID, Matches.Player2DeckID AS Player2DeckID, Format, Player1.FirstName AS "First Name", Player1.LastName AS "Last Name", Player2.FirstName AS "Opponent First Name",Player2.LastName AS "Opponent Last Name", Player1Deck.Name AS "Deck", Player2Deck.Name AS "Opponent Deck", Matches.Wins ,Matches.Losses, Matches.Ties, Matches.Date,Matches.Tournament 
+                    SELECT Matches.ID, Matches.Format, Player1Table.Username AS "Player1", Player2Table.Username AS "Player2", Player1Table.DeckName AS "Player1 Deck", Player1Table.DeckLink AS "Player1 Deck Link", Player2Table.DeckName AS "Player2 Deck", Player2Table.DeckLink AS "Player2 Deck Link", Matches.Wins ,Matches.Losses, Matches.Ties, Matches.Date, Matches.Tournament 
                     FROM Matches
-                    JOIN Players AS Player1 ON Matches.Player1ID = Player1.ID
-                    JOIN Players AS Player2 ON Matches.Player2ID = Player2.ID
-                    JOIN Decks AS Player1Deck ON Matches.Player1DeckID = Player1Deck.ID
-                    JOIN Decks AS Player2Deck ON Matches.Player2DeckID = Player2Deck.ID');//Prepares statement to inject ID into
+                    JOIN MatchParts AS Player1Table ON Player1Table.MatchID = Matches.ID AND Player1Table.OrderedFirst = 1
+                    JOIN MatchParts AS Player2Table ON Player2Table.MatchID = Matches.ID AND Player2Table.OrderedFirst = 0
+                    WHERE Matches.ID = ?;');//Prepares statement to inject ID into
   
             if($preparedStatement->bind_param("i",$id)==false)
             {
@@ -120,22 +132,26 @@
         
         public function addMatch($match)
         {
+            if($this->sql==NULL)
+            {
+                $this->error="No Database Connection";
+                return array($matches,$this->error)
+            }//If No Connection to Database
+            
             if($this->sql->connect_error!=null)
             {
                 $this->error=$this->sql->connect_error;
                 return $this->error;
             }//If Connection Error
             
-            $player1FirstName=$match['player1FirstName'];
-            $player1LastName=($match['player1LastName']!=NULL?$match['player1LastName']:"");//Default Blank if Not Specified
+            $player1LastName=($match['player1Username'];
             $player1DeckName=$match['player1DeckName'];
-            $player1MainBoard=($match['player1MainBoard']!=NULL?$match['player1MainBoard']:"");//Default Blank if Not Specified
-            $player1SideBoard=($match['player1SideBoard']!=NULL?$match['player1SideBoard']:"");//Default Blank if Not Specified
-            $player2FirstName=($match['player2FirstName']!=NULL?$match['player2LastName']:"");//Default Blank if Not Specified
-            $player2LastName=($match['player1LastName']!=NULL?$match['player2LastName']:"");//Default Blank if Not Specified
+            $player1DeckLink=$match['player1DeckLink'];
+            $player1OrderedFirst=$match['player1OrderedFirst'];
+            $player2LastName=($match['player2Username'];
             $player2DeckName=$match['player2DeckName'];
-            $player2MainBoard=($match['player2MainBoard']!=NULL?$match['player2MainBoard']:"");//Default Blank if Not Specified
-            $player2SideBoard=($match['player2SideBoard']!=NULL?$match['player2SideBoard']:"");//Default Blank if Not Specified
+            $player2DeckLink=$match['player2DeckLink'];
+            $player1OrderedFirst=$match['player2OrderedFirst'];
             $wins=$match['wins'];
             $losses=$match['losses'];
             $ties=($match['ties']!=NULL?$match['ties']:0);//Default 0 Ties is not specified
